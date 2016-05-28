24567a0a86d75e86dbfecf9a4218f50fbfa0cb36
Rémy BRILLET <remybrillet@gmail.com>
2016-05-28 18:21:38 +0200

    ajout action / user

--- js/user.js
+++ js/user.js
@@ -1,5 +1,4 @@
 var login = function(email, password, success, fail) {
-
 	firebase.auth().signInWithEmailAndPassword(email, password).catch(fail);
 	var user = firebase.auth().currentUser;
 	if(user) {
@@ -10,9 +9,7 @@ var login = function(email, password, success, fail) {
 }
 
 var logout = function() {
-
-var user = firebase.auth().currentUser;
-
+	var user = firebase.auth().currentUser;
 	if (user) 
 	{
 		firebase.auth().signOut();
@@ -21,28 +18,21 @@ var user = firebase.auth().currentUser;
 }
 
 var createUser = function(email, password, fail) {
-
 	firebase.auth().createUserWithEmailAndPassword(email, password).catch(fail);
 }
 
 var updateUserPassword = function(password, success, fail) {
-
-var user = firebase.auth().currentUser;
-user.updatePassword(password).then(success, fail);
-
+	var user = firebase.auth().currentUser;
+	user.updatePassword(password).then(success, fail);
 }
 
 var updateUserEmail = function(email, success, fail) {
-
-var user = firebase.auth().currentUser;
-user.updateEmail(email).then(success, fail);
-
+	var user = firebase.auth().currentUser;
+	user.updateEmail(email).then(success, fail);
 }
 
 var passwordReset = function(email, success, fail) {
-
-firebase.auth().sendPasswordResetEmail(email).then(success, fail);
-
+	firebase.auth().sendPasswordResetEmail(email).then(success, fail);
 }
 
 var updateUserName = function(username, success, fail) {
@@ -57,6 +47,16 @@ var updateUserPhotoURL = function(photoURL, success, fail) {
 	var user = firebase.auth().currentUser;
 	var profile = {displayName: user.displayName, photoURL: photoURL};
 	user.updateProfile(profile).then(success, fail);
+}
+
+var vote = function(action) {
+	var user = firebase.auth().currentUser;
+	var index = action.voters.indexOf(user.uid);
+	if(index) return; // if user already voted, cancel
+
+	action.voters.push(user.uid);
+	action.rank++;
+	firebase.database().ref('action/' + Object.keys(action)[0]).update(action);
 }
 
 var displayMsg = function(message) {
