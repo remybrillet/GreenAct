8bc26201878c9433b5263fc3917cccb0a4bfd590
thomas <thomas.jomeau@gmail.com>
2016-05-29 11:04:02 +0200

    Sync avancement. Page d'accueil + page d'act + page profil

--- js/user.js
+++ js/user.js
@@ -9,10 +9,27 @@ var login = function(email, password, success, fail) {
 	}
 }
 
+var loginWithProvider = function(provider) {
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+	  // This gives you a Google Access Token. You can use it to access the Google API.
+	  var token = result.credential.accessToken;
+	  // The signed-in user info.
+	  var user = result.user;
+	  // ...
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+	});
+}
+
 var logout = function() {
-
-var user = firebase.auth().currentUser;
-
+	var user = firebase.auth().currentUser;
 	if (user) 
 	{
 		firebase.auth().signOut();
@@ -21,15 +38,12 @@ var user = firebase.auth().currentUser;
 }
 
 var createUser = function(email, password, fail) {
-
 	firebase.auth().createUserWithEmailAndPassword(email, password).catch(fail);
 }
 
 var updateUserPassword = function(password, success, fail) {
-
-var user = firebase.auth().currentUser;
-user.updatePassword(password).then(success, fail);
-
+	var user = firebase.auth().currentUser;
+	user.updatePassword(password).then(success, fail);
 }
 
 var updateUserEmail = function(email, success, fail) {
